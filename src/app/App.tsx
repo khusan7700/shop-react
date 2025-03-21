4208311460598aa7ae6be8b1f6320546b7ee5b76
Khusan <khusan7700@gmail.com>
2025-03-21 23:35:18 +0900

    feat: develop screen based components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,49 @@
 import React from "react";
 
 import "../css/app.css";
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import { Link, Route, Routes } from "react-router-dom";
+import { HomePage } from "../screens/homePage";
+import { ProductsPage } from "../screens/productsPage";
+import { OrdersPage } from "../screens/ordersPage";
+import { UserPage } from "../screens/userPage";
 
 function App() {
-  return <div>home</div>;
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">HomePage</Link>
+          </li>
+          <li>
+            <Link to="/products">ProductsPage</Link>
+          </li>
+          <li>
+            <Link to="/orders">OrdersPage</Link>
+          </li>
+          <li>
+            <Link to="/member-page">UserPage</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/products">
+          <ProductsPage />
+        </Route>
+        <Route path="/orders">
+          <OrdersPage />
+        </Route>
+        <Route path="/member-page">
+          <UserPage />
+        </Route>
+        <Route path="/">
+          <HomePage />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,11 @@ import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./app/App";
 import reportWebVitals from "./reportWebVitals";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
 import "./css/index.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import theme from "./MaterialTheme";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
@@ -12,7 +16,12 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Router>
+          <App />
+        </Router>
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
