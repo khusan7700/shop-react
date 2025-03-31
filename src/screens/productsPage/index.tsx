f969e943a25c1ae9db5091aed9a47c2f308035c0
Khusan <khusan7700@gmail.com>
2025-03-31 20:18:12 +0900

    feat: develop product component  page mobile version

--- src/screens/productsPage/index.tsx
+++ src/screens/productsPage/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Products from "./Products";
+import ChosenProduct from "./ChosenProduct";
 
 export default function ProductsPage() {
   return (
     <div className={"products-page"}>
       <Products />
+      {/* <ChosenProduct /> */}
     </div>
   );
 }
