46d0354319101baaa52f7c86f85bc42469458ace
Khusan <khusan7700@gmail.com>
2025-05-20 13:46:03 +0900

    feat: create homepage reudex configuration

--- src/screens/homePage/index.tsx
+++ src/screens/homePage/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PopularDishes from "./PopularDishes";
 import NewDishes from "./NewDishes";
 import Advertisement from "./Adcertisement";
@@ -7,6 +7,13 @@ import Events from "./Events";
 import "../../css/home.css";
 
 export default function HomePage() {
+  //Selector: Store => DATA
+
+  useEffect(() => {
+    //Backend server data request => Data
+    // Slice: Data --> Store
+  }, []);
+
   return (
     <div className={"home-page"}>
       <PopularDishes />
