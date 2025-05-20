ee1fae58ca04e96be805a6300dc8c52fc8f94588
Khusan <khusan7700@gmail.com>
2025-05-20 14:20:26 +0900

    fix: develop slice and seletor usega

--- src/screens/homePage/index.tsx
+++ src/screens/homePage/index.tsx
@@ -6,8 +6,25 @@ import ActiveUsers from "./ActiveUsers";
 import Events from "./Events";
 import "../../css/home.css";
 
+import { useDispatch, useSelector } from "react-redux";
+import { Dispatch } from "@reduxjs/toolkit";
+import { createSelector } from "reselect";
+import { setPopularDishes } from "./slice";
+import { retrievePopularDishes } from "./selector";
+import { Product } from "../../lib/types/product";
+
+/** REDUX SLICE & SELECTOR **/
+const actionDispatch = (dispatch: Dispatch) => ({
+  setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
+});
+const popularDishesRetriever = createSelector(
+  retrievePopularDishes,
+  (popularDishes) => ({ popularDishes })
+);
+
 export default function HomePage() {
-  //Selector: Store => DATA
+  const { setPopularDishes } = actionDispatch(useDispatch());
+  const { popularDishes } = useSelector(popularDishesRetriever);
 
   useEffect(() => {
     //Backend server data request => Data
